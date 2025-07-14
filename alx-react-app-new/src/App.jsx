70ab19315745eb7a347b988781b64ce2c0c97fad
chidiogboi <chimanda01@gmail.com>
2025-07-14 19:13:48 +0000

    Fix: Move components to src/components/ for ALX checks

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,4 +1,8 @@
 // src/App.jsx
+import Header from './components/Header';
+import UserProfile from './components/UserProfile';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 import ProfilePage from './ProfilePage';
 import UserContext from './UserContext';
 
