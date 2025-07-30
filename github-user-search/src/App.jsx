472b9bcb25c69e3db5085634f9adc793e0233b03
chidiogboi <chimanda01@gmail.com>
2025-07-30 16:03:12 -0600

    Initial setup of GitHub User Search application

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>GitHub User Search</h1>
+        <p>Welcome to the GitHub User Search Application!</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
