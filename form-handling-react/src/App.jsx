805f7098da34744d1d845bcac196d685f5673c16
chidiogboi <chimanda01@gmail.com>
2025-08-18 17:49:36 -0600

    Initial setup for form-handling-react project with basic styling

---
+++ form-handling-react/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './index.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Form Handling in React</h1>
+      <p style={{textAlign: 'center', marginBottom: '30px', color: '#666'}}>
+        Learn controlled components and Formik for form handling
+      </p>
+      {/* We'll add components here */}
+    </div>
+  );
+}
+
+export default App;
