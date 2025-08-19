f6973c279cc67e3083aa574cdbac51177bb3b103
chidiogboi <chimanda01@gmail.com>
2025-08-18 20:04:52 -0600

    Task 2: Added dynamic route /blog/:id with BlogPost component

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link, useParams } from 'react-router-dom';
 
 function Home() {
   return <h1 className="text-3xl font-bold text-center mt-10">Home Page</h1>;
@@ -6,6 +6,16 @@ function Home() {
 
 function About() {
   return <h1 className="text-3xl font-bold text-center mt-10">About Page</h1>;
+}
+
+function BlogPost() {
+  const { id } = useParams(); // Get the dynamic :id parameter from the URL
+  return (
+    <div className="mt-5 text-center">
+      <h1 className="text-3xl font-bold">Blog Post</h1>
+      <p className="mt-3">Viewing blog post with ID: {id}</p>
+    </div>
+  );
 }
 
 function App() {
@@ -16,11 +26,13 @@ function App() {
           <ul className="flex space-x-4 justify-center">
             <li><Link to="/" className="text-white hover:text-gray-200">Home</Link></li>
             <li><Link to="/about" className="text-white hover:text-gray-200">About</Link></li>
+            <li><Link to="/blog/123" className="text-white hover:text-gray-200">Blog Post</Link></li>
           </ul>
         </nav>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/blog/:id" element={<BlogPost />} />
         </Routes>
       </div>
     </BrowserRouter>
